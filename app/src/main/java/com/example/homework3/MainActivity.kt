c05b0051ecc56a7071fed2d26404ecd34c548f23
VladimirSerkutyev <serkutyevv@gmail.com>
2022-04-22 00:57:50 +0300

    Revert "HW3_Kotlin_Serkutyev_Vladimir"
    This reverts commit e282e254

--- app/src/main/java/com/example/homework3/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.homework3
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.homework3.fragments.ListFragment
-
-class MainActivity : AppCompatActivity(R.layout.activity_main)
--- app/src/main/java/com/example/homework3/adapters/LoadingViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.example.homework3.adapters
-
-import androidx.recyclerview.widget.RecyclerView
-import com.example.homework3.databinding.ItemLoadingBinding
-
-class LoadingViewHolder(binding: ItemLoadingBinding) : RecyclerView.ViewHolder(binding.root)
